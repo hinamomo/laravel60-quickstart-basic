c569a89ec835c473c2af2aee04b26bb0b253aab2
hoge <hoge@gmail.com>
2020-01-11 15:01:49 +0000

    アクセストークン発行　途中

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,18 +28,34 @@ class HomeController extends Controller
         // TODO エラーレスポンスが返って来た場合は適切に処理する必要がある
         $error_description = $request->input('error_description');
         if($error_description != ""){
-            //if($error_description == "The+resource+owner+denied+the+request."){
                 \Session::flash('flash_message', '権限拒否られたー');
                 return redirect('/');
-            //}
         }
         
+        // アクセストークンを発行する
+        basic_request(); 
         
-//        return view('home');
+
         $tasks = Task::orderBy('created_at','asc')->get();
     
         return view('tasks',[
             'tasks' => $tasks
         ]);
+    }
+    
+    // アクセストークン発行
+    public function basic_request() {
+        $base_url = 'http://example.com';
+        $client = new \GuzzleHttp\Client( [
+        'base_uri' => $base_url,
+        ] );
+
+        $path = '/index.html';
+        $response = $client->request( 'GET', $path,
+        [
+            'allow_redirects' => true,
+        ] );
+        $response_body = (string) $response->getBody();
+        echo $response_body;
     }
 }
