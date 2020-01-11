af40aa741afda1c90905ada769ec930ff30e0a79
hoge <hoge@gmail.com>
2020-01-11 14:44:31 +0000

    権限拒否時の処理のTODO

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,10 +19,19 @@ class HomeController extends Controller
         $state_line = $request->input('state');
         $state_cookie = \Cookie::get('state');
         
+        // stateが異なる場合
         if($state_line !== $state_cookie){
             \Session::flash('flash_message', 'state検証エラー');
             return redirect('/');
-//            return redirect('/')->with('flash_message', 'state検証エラー');
+        }
+        
+        // TODO エラーレスポンスが返って来た場合は適切に処理する必要がある
+        $error_description = $request->input('error_description');
+        if($error_description != ""){
+            //if($error_description == "The+resource+owner+denied+the+request."){
+                \Session::flash('flash_message', '権限拒否られたー');
+                return redirect('/');
+            //}
         }
         
         
